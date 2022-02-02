ce8e409fb2b1c596c5dcaea9a6ffe137ead3fb88
Zouhir OUFTOU <zouhir.ouftou@gmail.com>
2022-02-02 20:33:59 +0100

    push exercises

--- hello-world.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main(int argc, char const *argv[])
-{
-    printf("hello, world\n");
-    printf("\community\by\n");
-    
-    return 0;
-}
--- lab.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main(int argc, char const *argv[])
-{
-
-    printf("%d\n",EOF);
-    
-    return 0;
-}
